4c553fb642dca6e6eb784c16aa7edef7c46df6bb
Avicenna <87450618+AvicennaJr@users.noreply.github.com>
2022-06-19 00:15:01 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d\n", number);
+    
+    return 0;
+}
